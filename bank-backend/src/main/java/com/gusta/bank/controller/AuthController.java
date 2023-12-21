13a6a92b7a6295493faa79ffdb299782eaf37a8a
gustavo-angeli <gustavoangeli.silva@outlook.com>
2023-12-21 17:57:26 -0300

    docs: Improving API Docs

--- bank-backend/src/main/java/com/gusta/bank/controller/AuthController.java
+++ bank-backend/src/main/java/com/gusta/bank/controller/AuthController.java
@@ -3,24 +3,57 @@ package com.gusta.bank.controller;
 import com.gusta.bank.security.domain.dto.TokenDTO;
 import com.gusta.bank.security.domain.dto.UserDTO;
 import com.gusta.bank.service.AuthService;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import utils.ParameterValidation;
 
+@Tag(name = "Authentication", description = "Authentication endpoints")
 @RestController
 @RequestMapping(value = "/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService service;
 
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = UserDTO.class),
+                    examples = @ExampleObject(
+                            name = "example",
+                            value = "{\"email\":\"admin@admin.com\", \"password\":\"1234\"}"
+                    )
+            )
+    )
+    @ApiResponse(
+            responseCode = "200",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = TokenDTO.class)
+            )
+    )
     @PostMapping
     public ResponseEntity<TokenDTO> login(@RequestBody UserDTO userDTO) {
         ParameterValidation.nullOrEmptyParam(userDTO.getEmail(), userDTO.getPassword());
         return new ResponseEntity<>(service.login(userDTO), HttpStatus.OK);
     }
 
+    @ApiResponse(
+            responseCode = "200",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = TokenDTO.class)
+            )
+    )
     @PutMapping
     public ResponseEntity<TokenDTO> refresh(@RequestHeader(value = "Authorization") String token) {
         return new ResponseEntity<>(service.refreshToken(token), HttpStatus.OK);
--- bank-backend/src/main/java/com/gusta/bank/controller/EntityManagementController.java
+++ bank-backend/src/main/java/com/gusta/bank/controller/EntityManagementController.java
@@ -1,21 +1,45 @@
 package com.gusta.bank.controller;
 
+import com.gusta.bank.security.domain.dto.TokenDTO;
 import com.gusta.bank.security.domain.dto.UserDTO;
 import com.gusta.bank.service.EntityManagementService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import utils.ParameterValidation;
 
 import java.util.UUID;
 
+@Tag(name = "Management", description = "Management endpoints")
 @RestController
 @RequestMapping(value = "/api/v1/management")
 @RequiredArgsConstructor
 public class EntityManagementController {
     private final EntityManagementService service;
 
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = UserDTO.class),
+                    examples = @ExampleObject(
+                            name = "example",
+                            value = "{\"username\": \"example\", \"email\":\"example@example.com\", \"password\":\"1234\"}"
+                    )
+            )
+    )
+    @ApiResponse(
+            responseCode = "204",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE
+            )
+    )
     @PostMapping
     public ResponseEntity<HttpStatus> create(@RequestBody UserDTO userDTO) {
         ParameterValidation.nullOrEmptyParam(userDTO.getUsername(), userDTO.getEmail(), userDTO.getPassword());
@@ -23,17 +47,26 @@ public class EntityManagementController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @ApiResponse(
+            responseCode = "204",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE
+            )
+    )
     @DeleteMapping
     public ResponseEntity<HttpStatus> delete(@RequestHeader(value = "Authorization") String token) {
         ParameterValidation.nullOrEmptyParam(token);
-        System.out.println(UUID.randomUUID());
-        System.out.println(UUID.randomUUID());
-
         service.deleteByToken(token);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     //ENDPOINTS THAT ONLY USERS WITH ROLE_ADMIN CAN ACCESS
+    @ApiResponse(
+            responseCode = "204",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE
+            )
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<HttpStatus> deleteById(@PathVariable(value = "id") String id) {
         ParameterValidation.nullOrEmptyParam(id);
